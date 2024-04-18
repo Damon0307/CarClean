4c627c25c6b55319d03b8d372512009bfcaa9834
wjc <1294056177@qq.com>
2024-04-18 20:55:36 +0800

    优化编译规则

--- build/CMakeFiles/CarClean.dir/DependInfo.cmake
+++ build/CMakeFiles/CarClean.dir/DependInfo.cmake
@@ -1,23 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/opt/WorkingRoom/CarClean/src/DirectorLinkClient.cpp" "/opt/WorkingRoom/CarClean/build/CMakeFiles/CarClean.dir/src/DirectorLinkClient.cpp.o"
-  "/opt/WorkingRoom/CarClean/src/NetFoundation.cpp" "/opt/WorkingRoom/CarClean/build/CMakeFiles/CarClean.dir/src/NetFoundation.cpp.o"
-  "/opt/WorkingRoom/CarClean/src/UartMod.cpp" "/opt/WorkingRoom/CarClean/build/CMakeFiles/CarClean.dir/src/UartMod.cpp.o"
-  "/opt/WorkingRoom/CarClean/src/WashReport.cpp" "/opt/WorkingRoom/CarClean/build/CMakeFiles/CarClean.dir/src/WashReport.cpp.o"
-  "/opt/WorkingRoom/CarClean/src/main.cpp" "/opt/WorkingRoom/CarClean/build/CMakeFiles/CarClean.dir/src/main.cpp.o"
-  "/opt/WorkingRoom/CarClean/src/tinyxml2.cpp" "/opt/WorkingRoom/CarClean/build/CMakeFiles/CarClean.dir/src/tinyxml2.cpp.o"
-  "/opt/WorkingRoom/CarClean/src/uart.cpp" "/opt/WorkingRoom/CarClean/build/CMakeFiles/CarClean.dir/src/uart.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../src"
-  "../src/spdlog"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sam/WorkingRoom/CarClean/src/DirectorLinkClient.cpp" "CMakeFiles/CarClean.dir/src/DirectorLinkClient.cpp.o" "gcc" "CMakeFiles/CarClean.dir/src/DirectorLinkClient.cpp.o.d"
+  "/home/sam/WorkingRoom/CarClean/src/NetFoundation.cpp" "CMakeFiles/CarClean.dir/src/NetFoundation.cpp.o" "gcc" "CMakeFiles/CarClean.dir/src/NetFoundation.cpp.o.d"
+  "/home/sam/WorkingRoom/CarClean/src/UartMod.cpp" "CMakeFiles/CarClean.dir/src/UartMod.cpp.o" "gcc" "CMakeFiles/CarClean.dir/src/UartMod.cpp.o.d"
+  "/home/sam/WorkingRoom/CarClean/src/WashReport.cpp" "CMakeFiles/CarClean.dir/src/WashReport.cpp.o" "gcc" "CMakeFiles/CarClean.dir/src/WashReport.cpp.o.d"
+  "/home/sam/WorkingRoom/CarClean/src/main.cpp" "CMakeFiles/CarClean.dir/src/main.cpp.o" "gcc" "CMakeFiles/CarClean.dir/src/main.cpp.o.d"
+  "/home/sam/WorkingRoom/CarClean/src/tinyxml2.cpp" "CMakeFiles/CarClean.dir/src/tinyxml2.cpp.o" "gcc" "CMakeFiles/CarClean.dir/src/tinyxml2.cpp.o.d"
+  "/home/sam/WorkingRoom/CarClean/src/uart.cpp" "CMakeFiles/CarClean.dir/src/uart.cpp.o" "gcc" "CMakeFiles/CarClean.dir/src/uart.cpp.o.d"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/CarClean.dir/cmake_clean.cmake
+++ build/CMakeFiles/CarClean.dir/cmake_clean.cmake
@@ -1,11 +1,18 @@
 file(REMOVE_RECURSE
   "CMakeFiles/CarClean.dir/src/DirectorLinkClient.cpp.o"
+  "CMakeFiles/CarClean.dir/src/DirectorLinkClient.cpp.o.d"
   "CMakeFiles/CarClean.dir/src/NetFoundation.cpp.o"
+  "CMakeFiles/CarClean.dir/src/NetFoundation.cpp.o.d"
   "CMakeFiles/CarClean.dir/src/UartMod.cpp.o"
+  "CMakeFiles/CarClean.dir/src/UartMod.cpp.o.d"
   "CMakeFiles/CarClean.dir/src/WashReport.cpp.o"
+  "CMakeFiles/CarClean.dir/src/WashReport.cpp.o.d"
   "CMakeFiles/CarClean.dir/src/main.cpp.o"
+  "CMakeFiles/CarClean.dir/src/main.cpp.o.d"
   "CMakeFiles/CarClean.dir/src/tinyxml2.cpp.o"
+  "CMakeFiles/CarClean.dir/src/tinyxml2.cpp.o.d"
   "CMakeFiles/CarClean.dir/src/uart.cpp.o"
+  "CMakeFiles/CarClean.dir/src/uart.cpp.o.d"
   "CarClean"
   "CarClean.pdb"
 )
