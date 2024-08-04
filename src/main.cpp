325ce22f6098abb421f4e152a7a621079f4f4b61
wjc <1294056177@qq.com>
2024-08-04 22:24:56 +0800

    单雷达开始

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ const char *NET_CFG_FILE = "net_cfg.json";
 const char *DEF_CFG_FILE = "default_info.json";
 const char *DIRECT_LINK_CFG_FILE = "direct_link.json";
 
-const char *version_str = "Version AI 5s 闸机联动(delay+keep) +自动重启";
+const char *version_str = "Version  单雷达  AI 5s 闸机联动(delay+keep time)";
 
 std::shared_ptr<spdlog::logger> g_console_logger;
 std::shared_ptr<spdlog::logger> g_file_logger;
